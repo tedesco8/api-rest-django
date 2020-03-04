e4e8e5ef6e0c8ec5e6d2d94eade393b77f7192b8
Pablo Tedesco <tedesco8@hotmail.com>
2020-03-04 01:57:36 -0300

    commit para gonzalo

--- projectVue/src/App.vue
+++ projectVue/src/App.vue
@@ -111,7 +111,7 @@
               </v-list-item-action>
               <v-list-item-content>
                 <v-list-item-title>
-                  Clientes
+                  Cliente
                 </v-list-item-title>
               </v-list-item-content>
             </v-list-item>
