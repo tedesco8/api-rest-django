996ab4820b7b14a398a2b4f2a99407c065b5f9af
Pablo Tedesco <tedesco8@hotmail.com>
2020-03-20 02:09:42 -0300

    trabajando en componente home del blog

--- projectblog/src/views/Home.vue
+++ projectblog/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!--Container centra todo nuestro contenido, con el atributo fluid se pega mas a los bordes-->
+  <v-container fluid>
+    <!--Layout funciona como el row de bootstrap, para trabajar con una fila y el sistema. Wrap es para que no se pise y respete el sistema-->
+    <v-layout class = "secondary" justify-center align-center style="height:300px">
+      <v-flex class="info" md3>
+        <h2>Centrado</h2>
+      </v-flex>
+      <v-flex class="success" md3>
+        <h2>Centrado</h2>
+      </v-flex>
+    </v-layout>
+    <v-layout wrap>
+      <v-flex class="pink lighten" md6>
+          <h1>
+            Probando grilla
+          </h1>
+      </v-flex>
+      <v-flex class="purple darken-4" md6>
+        <h1>
+            Probando grilla
+          </h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
