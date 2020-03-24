ac67651f0699b8eb5c08472f60fe1bf5148d9757
Pablo Tedesco <tedesco8@hotmail.com>
2020-03-24 00:43:24 -0300

    error en cards solucionado. Falta renderizar bien post en cards

--- projectblog/src/components/Cards.vue
+++ projectblog/src/components/Cards.vue
@@ -1,80 +1,45 @@
 <template>
-    <div>
         <!--Container centra todo nuestro contenido, con el atributo fluid se pega mas a los bordes-->
         <v-container grid-list-md>
             <v-layout row wrap>
                 <v-flex xs12 sm6 md3>
-                <v-card>
-                    <v-img :src="require('../assets/NMC1.png')">
-
-                    </v-img>
-                    <v-card-title>
-
-                    </v-card-title>
-                    <v-card-text>
-
-                    </v-card-text>
-                    <v-card-actions>
-                    
-                    </v-card-actions>
-                </v-card>
-                </v-flex>
-                <v-flex xs12 sm6 md3>
-                <v-card>
-                    <v-img :src="require('../assets/NMC2.png')">
-
-                    </v-img>
-                    <v-card-title>
-
-                    </v-card-title>
-                    <v-card-text>
-
-                    </v-card-text>
-                    <v-card-actions>
-                    
-                    </v-card-actions>
-                </v-card>
-                </v-flex>
-                <v-flex xs12 sm6 md3>
-                <v-card>
-                    <v-img :src="require('../assets/NMC3.png')">
-
-                    </v-img>
-                    <v-card-title>
-
-                    </v-card-title>
-                    <v-card-text>
-
-                    </v-card-text>
-                    <v-card-actions>
-                    
-                    </v-card-actions>
-                </v-card>
-                </v-flex>
-                <v-flex xs12 sm6 md3>
-                <v-card>
-                    <v-img :src="require('../assets/NMC4.png')">
-
-                    </v-img>
-                    <v-card-title>
-
-                    </v-card-title>
-                    <v-card-text>
-
-                    </v-card-text>
-                    <v-card-actions>
-                    
-                    </v-card-actions>
-                </v-card>
+                    <v-card v-for="(item, index) in post" v-bind:key="item">
+                        <v-img>
+                           {{index}}. {{post.cover}}
+                        </v-img>
+                        <v-card-title>
+                            <h2>{{post.title}}</h2>
+                        </v-card-title>
+                        <v-card-text>
+                            <p>{{post.description}}</p>
+                        </v-card-text>
+                        <v-card-actions>
+                        
+                        </v-card-actions>
+                    </v-card>
                 </v-flex>
             </v-layout>
         </v-container>
-    </div>
 </template>
 
 <script>
     export default {
-        name: 'Cards'
+        name: 'Cards',
+        data() {
+            return {
+                post: [
+                    {
+                        title: 'Fotos que nos llegan desde el Este!',
+                        description: 'Personas comprometidas con las playas, que depositan sus colillas en los ceniceros de @teko.uy',
+                        cover: require('../assets/NMC1.png')
+                    },
+                    {
+                        title: 'Hoy cubrimos nuestro primer evento en Montevideo',
+                        description: 'Teniendo como resultado la recolección de más de 1000 colillas de cigarros que se encontraban en el Parque Rodó'
+                    }
+                ]
+            }
+        }
     }
 </script>
 
--- projectblog/src/views/Home.vue
+++ projectblog/src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
-  <!--SLIDER-->
-    <Slider />
-  <!--MAPA-->
-    <Map />
-  <!--CARDS-->
-    <Card />
+  <v-fragment>
+    <!--SLIDER-->
+      <Slider />
+    <!--MAPA-->
+      <Map />
+    <!--CARDS-->
+      <Card />
+  </v-fragment>
 </template>
 
 <script>
-  import Map from './Maps'
-  import Slider from './Slider'
-  import Card from './Cards'
+  import Map from '../components/Maps.vue'
+  import Slider from '../components/Slider.vue'
+  import Card from '../components/Cards.vue'
 
   export default {
     components: {
